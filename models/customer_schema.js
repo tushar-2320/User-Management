3274553fd755e7c763e3db9e61d4f279a56c4b9d
Tushar Kumar <tusharkumar@Tushars-MacBook-Air.local>
2024-11-17 18:04:37 +0530

    fix minor issue

--- models/customer_schema.js
+++ models/customer_schema.js
@@ -15,12 +15,12 @@ const Schema= new mongoose.Schema({
     email:{
         type:String,
         required:true,
-       // unique:true,
+        unique:true,
     },
     phoneNumber:{
         type:Number,
         required:true,
-        //unique:true,
+        unique:true,
     },
     company:{
         type:String,
